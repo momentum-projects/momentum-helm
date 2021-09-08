c4cb5eb5289685e93768de96a513cfc048ed4752
Chris Athanas <chris@crowdskout.com>
2021-09-08 14:21:37 -0500

    Tictactoe game

--- resume-chris/src/app/app.component.ts
+++ resume-chris/src/app/app.component.ts
@@ -5,29 +5,180 @@ import Profile, { profiles } from './profile.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+// export class AppComponent implements OnInit {
+//   title = 'resume-chris';
+//   profiles: Profile[] = profiles;
+
+//   ngOnInit() {
+//     setTimeout(() => {
+//       this.firstProfileSet = 'Jin';
+//       this.profiles[1].firstName = 'Thomans';
+//     }, 1000);
+//   }
+
+//   firstProfile() {
+//     return this.profiles[0];
+//   }
+
+//   get firstProfileGet() {
+//     return this.firstProfile();
+//   }
+//   set firstProfileSet(value: string) {
+//     this.firstProfile().firstName = value;
+//   }
+
+//   onClick() {
+//     this.firstProfileSet = 'Jimbo';
+//   }
+// }
+
 export class AppComponent implements OnInit {
-  title = 'resume-chris';
-  profiles: Profile[] = profiles;
+  whoTurn;
+  gameWon;
+  board;
+  badMove;
 
   ngOnInit() {
-    setTimeout(() => {
-      this.firstProfileSet = 'Jin';
-      this.profiles[1].firstName = 'Thomans';
-    }, 1000);
+    this.onStartOver();
+    // setTimeout(() => {
+    //   this.board = [
+    //     ['❎', '🟩', '❎'],
+    //     ['❎', '⏺', '⏺'],
+    //     ['🟩', '🟩', '⏺'],
+    //   ];
+    // }, 1000);
   }
 
-  firstProfile() {
-    return this.profiles[0];
+  onStartOver() {
+    this.board = [
+      ['🟩', '🟩', '🟩'],
+      ['🟩', '🟩', '🟩'],
+      ['🟩', '🟩', '🟩'],
+    ];
+    this.gameWon = '';
+    this.whoTurn = '❎';
   }
 
-  get firstProfileGet() {
-    return this.firstProfile();
-  }
-  set firstProfileSet(value: string) {
-    this.firstProfile().firstName = value;
+  setPlayerTurn(player: string): string {
+    if (player == '❎') return '⏺';
+    return '❎';
   }
 
-  onClick() {
-    this.firstProfileSet = 'Jimbo';
+  onSetLocation(player: string, x: number, y: number) {
+    if (this.gameWon) return;
+    if (this.board[x][y] != '🟩') {
+      this.badMove = true;
+      return;
+    };
+    this.badMove = false;
+    this.board[x][y] = player;
+    this.whoTurn = this.setPlayerTurn(this.whoTurn);
+    this.checkForWinner();
+  }
+
+  checkForWinner() {
+    this.gameWon = '';
+    let playerX = 0;
+    let playerO = 0;
+
+    // vertical
+    for (let x = 0; x < 3; x++) {
+      playerX = 0;
+      playerO = 0;
+      for (let y = 0; y < 3; y++) {
+        if (this.board[y][x] == '❎') {
+          playerX++;
+        } else if (this.board[y][x] == '⏺') {
+          playerO++;
+        }
+
+        if (playerX == 3) {
+          this.gameWon = '❎';
+        } else if (playerO == 3) {
+          this.gameWon = '⏺';
+        }
+
+        if (this.gameWon) return;
+      }
+    }
+
+    // horizontal
+    for (let y = 0; y < 3; y++) {
+      playerX = 0;
+      playerO = 0;
+      for (let x = 0; x < 3; x++) {
+        if (this.board[y][x] == '❎') {
+          playerX++;
+        } else if (this.board[y][x] == '⏺') {
+          playerO++;
+        }
+
+        if (playerX == 3) {
+          this.gameWon = '❎';
+        } else if (playerO == 3) {
+          this.gameWon = '⏺';
+        }
+
+        if (this.gameWon) return;
+      }
+    }
+
+    // Diagonal to right
+    let x = 0;
+    playerX = 0;
+    playerO = 0;
+    for (let y = 0; y < 3; y++) {
+      if (this.board[y][x] == '❎') {
+        playerX++;
+      } else if (this.board[y][x] == '⏺') {
+        playerO++;
+      }
+
+      if (playerX == 3) {
+        this.gameWon = '❎';
+      } else if (playerO == 3) {
+        this.gameWon = '⏺';
+      }
+
+      if (this.gameWon) return;
+      x++;
+    }
+
+    // Diagonal from right
+    x = 2;
+    playerX = 0;
+    playerO = 0;
+    for (let y = 0; y < 3; y++) {
+      if (this.board[y][x] == '❎') {
+        playerX++;
+      } else if (this.board[y][x] == '⏺') {
+        playerO++;
+      }
+
+      if (playerX == 3) {
+        this.gameWon = '❎';
+      } else if (playerO == 3) {
+        this.gameWon = '⏺';
+      }
+
+      if (this.gameWon) return;
+      x--;
+    }
+
+    let slotsFull = 0;
+    for (let x = 0; x < 3; x++) {
+      for (let y = 0; y < 3; y++) {
+        if (this.board[y][x] != '🟩') {
+          slotsFull++
+        }
+      }
+    }
+    if (slotsFull == 9) {
+      this.gameWon = 'Nobody';
+    }
+  }
+
+  onConsoleLog(x) {
+    console.log(x);
   }
 }
