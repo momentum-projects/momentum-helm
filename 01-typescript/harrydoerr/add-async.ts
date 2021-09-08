bacb243249b069cfc204356a45d9180580262036
harrydoerr <harry@crowdskout.com>
2021-09-08 07:03:16 -0700

    Typescript practice work

---
+++ 01-typescript/harrydoerr/add-async.ts
@@ -0,0 +1,28 @@
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function ask(question: string): Promise<string> {
+    return new Promise(resolve => {
+        rl.question(question, resolve);
+    })
+}
+
+async function runAdd() {
+    try {
+        const val1 = await ask('first number: ');
+        const val2 = await ask('second number: ');
+        console.log("The sum is: " + (parseInt(val1) + parseInt(val2)))
+    }
+    catch (err) {
+        console.log("Question rejected");
+    }
+    finally {
+        rl.close();
+    }
+}
+
+runAdd().then(() => console.log("DONE"));
---
+++ 01-typescript/harrydoerr/add.ts
@@ -0,0 +1,18 @@
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let val1: string;
+let val2: string;
+
+rl.question("first number", answer => {
+    val1 = answer;
+    rl.question("second number: ", answer => {
+        val2 = answer;
+        console.log("The sum is: " + (parseInt(val1) + parseInt(val2)))
+        rl.close();
+    })
+})
