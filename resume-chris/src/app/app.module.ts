9015be7643a1cf1fd4a655f5a9fbafb25e62e222
Chris Athanas <chris@crowdskout.com>
2021-09-08 20:48:01 -0500

    add tictactoe1 as a component

--- resume-chris/src/app/app.module.ts
+++ resume-chris/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Tictactoe2Component } from './tictactoe2/tictactoe2.component';
+import { Tictactoe1Component } from './tictactoe1/tictactoe1.component';
 
 @NgModule({
-  declarations: [AppComponent, Tictactoe2Component],
+  declarations: [AppComponent, Tictactoe2Component, Tictactoe1Component],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
