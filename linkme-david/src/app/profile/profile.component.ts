4f9265833d538f77a10983369e7f2c552358a0e0
David Rasch <rasch@raschnet.com>
2021-09-09 18:12:25 -0400

    feat(day4): adding packages for apollo graphql client

--- linkme-david/src/app/profile/profile.component.ts
+++ linkme-david/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, SimpleChanges, OnDestroy } from '@angular/core';
 import { ProfilesService } from '../profiles.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-profile',
@@ -10,9 +11,15 @@ export class ProfileComponent {
   @Input() profile!: number;
   @Input() name!: string;
 
+  githubToken = environment.githubToken;
+
   constructor(public profilesService: ProfilesService) {}
 
   get profileObject() {
     return this.profilesService.getProfile(this.profile);
+  }
+
+  get repositories() {
+
   }
 }
