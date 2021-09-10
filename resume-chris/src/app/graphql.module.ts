eb528f449d37840f39a77251974a9bdca52bbff4
Chris Athanas <chris@crowdskout.com>
2021-09-10 10:39:49 -0500

    add graphql auth

--- resume-chris/src/app/graphql.module.ts
+++ resume-chris/src/app/graphql.module.ts
@@ -1,14 +1,25 @@
-import {NgModule} from '@angular/core';
-import {APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
-import {HttpLink} from 'apollo-angular/http';
+import { NgModule } from '@angular/core';
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { setContext } from '@apollo/client/link/context';
+import { environment } from 'src/environments/environment';
 
 const uri = 'https://api.github.com/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({uri}),
+    link: authLink.concat(httpLink.create({ uri })),
     cache: new InMemoryCache(),
   };
+}
+
+const authLink = setContext((_, { headers}) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: 'Bearer ' + environment.githubToken,
+    }
+  }
 }
 
 @NgModule({
