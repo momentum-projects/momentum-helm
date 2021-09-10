0a6d3542fe96205a3fc7a7730fc88cedd3eb5cda
David Rasch <rasch@raschnet.com>
2021-09-09 21:52:12 -0400

    Revert "feat(day4): graphql provider"
    This reverts commit 2311250dbf785f66feeba702925f387af64b916a.

--- linkme-david/src/app/app.module.ts
+++ linkme-david/src/app/app.module.ts
@@ -20,44 +20,6 @@ import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { ExperienceComponent } from './profile/experience.component';
 import { ProfilesService } from './profiles.service';
-import { createHttpLink, InMemoryCache } from '@apollo/client';
-import { HttpLink } from 'apollo-angular/http';
-import { setContext } from '@apollo/client/link/context';
-import { environment } from 'src/environments/environment';
-import { APOLLO_NAMED_OPTIONS, NamedOptions } from 'apollo-angular';
-
-// const httpLink = createHttpLink({
-//   uri: 'https://api.github.com/graphql',
-// });
-
-const authLink = setContext((_, { headers }) => {
-  const token = environment.githubToken;
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const ApolloProvider = {
-  provide: APOLLO_NAMED_OPTIONS, // <-- Different from standard initialization
-  useFactory(httpLink: HttpLink): NamedOptions {
-    return {
-      newClientName: {
-        // <-- this settings will be saved by name: newClientName
-        cache: new InMemoryCache(),
-        link: authLink.concat(
-          httpLink.create({
-            uri: 'https://o5x5jzoo7z.sse.codesandbox.io/graphql',
-          })
-        ),
-      },
-    };
-  },
-  deps: [HttpLink],
-};
 
 registerLocaleData(en);
 
@@ -76,11 +38,7 @@ registerLocaleData(en);
     NzMenuModule,
     NzTypographyModule,
   ],
-  providers: [
-    ApolloProvider,
-    { provide: NZ_I18N, useValue: en_US },
-    ProfilesService,
-  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }, ProfilesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- linkme-david/src/app/profile/profile.component.ts
+++ linkme-david/src/app/profile/profile.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, SimpleChanges, OnDestroy } from '@angular/core';
 import { ProfilesService } from '../profiles.service';
 import { environment } from '../../environments/environment';
-import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-profile',
@@ -14,35 +13,13 @@ export class ProfileComponent {
 
   githubToken = environment.githubToken;
 
-  constructor(
-    public profilesService: ProfilesService,
-    private apolloProvider: Apollo
-  ) {}
+  constructor(public profilesService: ProfilesService) {}
 
   get profileObject() {
     return this.profilesService.getProfile(this.profile);
   }
 
-  repositories() {
-    this.apolloProvider
-      .watchQuery({
-        query: gql`
-          query {
-            viewer {
-              repositories(first: 30, privacy: PUBLIC) {
-                nodes {
-                  name
-                  url
-                  stargazerCount
-                }
-              }
-            }
-          }
-        `,
-      })
-      .valueChanges.subscribe(({ data, loading }) => {
-        console.log(data);
-        console.log(loading);
-      });
+  get repositories() {
+
   }
 }
