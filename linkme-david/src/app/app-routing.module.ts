29f15358fa1623133a965d3129ef1e8665eaef85
David Rasch <rasch@raschnet.com>
2021-09-13 11:48:36 -0400

    feat(day5): routing for top-level components

--- linkme-david/src/app/app-routing.module.ts
+++ linkme-david/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnectionsComponent } from './connections/connections.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfilesComponent } from './profile/profiles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profiles',
+    component: ProfilesComponent, // this is the component with the <router-outlet> in the template
+    children: [
+      {
+        path: 'one', // child route path matches url like http://localhost:4200/profiles/one?id=4 (via queryParams)
+        component: ProfileComponent, // child route component that the router renders
+      },
+      {
+        path: ':id', // child route path matches url like http://localhost:4200/profiles/4 (via params)
+        component: ProfileComponent, // child route component that the router renders
+      },
+    ],
+  },
+  {
+    path: 'connections',
+    component: ConnectionsComponent, // this is the component with the <router-outlet> in the template
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
