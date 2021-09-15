92e2ad00fe44085b9afead2b3aee35053ebccf2e
harrydoerr <harry@crowdskout.com>
2021-09-15 12:30:09 -0700

    feat(DAY07): Adding prisma migrations, starting to build out query structure

--- resume-harry/backend/index.ts
+++ resume-harry/backend/index.ts
@@ -1,33 +1,106 @@
 import { PrismaClient } from "@prisma/client";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const prisma = new PrismaClient();
+const port = 4000;
+const typeDefs = `
+  type Profile {
+    id: Int!
+    experience: [String]
+    firstName: String
+    lastName: String
+    title: String
+    connections: [ProfileConnections]
+  }
 
-async function main() {
-  await prisma.user.create({
-    data: {
-      name: "Alice",
+  type ProfileConnections {
+    id: Int
+    profile: Profile
+    connection: Connection
+    profileId: Int
+    connectionId: Int
+  }
 
-      email: "alice@prisma.io",
+  type Connection {
+    id: Int!
+    outboundId: Int
+    inboundId: Int
+    connections: [ProfileConnections]
+  }
 
-      posts: {
-        create: { title: "Hello World" },
-      },
+  type Query {
+    allProfiles: [Profile!]!
+  }
 
-      profile: {
-        create: { bio: "I like turtles" },
-      },
+  type Query {
+    oneProfile(id: Int!): Profile
+  }
+
+  type Query {
+    getProfileConnections(profileId: Int!): [Connection]
+  }
+
+  type Query {
+    getConnections(profileId: Int!): [Connection]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allProfiles: () => {
+      return prisma.profile.findMany();
     },
-  });
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-
-      profile: true,
+    oneProfile: (id: number) => {
+      return prisma.profile.findUnique({
+        where: {
+          id: 1,
+        },
+      });
     },
-  });
+    getProfileConnections: (profileId: number) => {
+      return prisma.connection.findMany({
+        where: {
+          connections: {
+            every: {
+              profileId: { 
+                equals: 1 
+              },
+            },
+          },
+        },
+      });
+    },
+    getConnections: (profileId: number) => {
+      return prisma.connection.findMany({
+        where: {
+          inboundId: {
+            equals: 1,
+          },
+        },
+      });
+    },
+  },
+};
 
-  console.dir(allUsers, { depth: null });
-}
+export const schema = makeExecutableSchema({
+  resolvers,
+  typeDefs,
+});
 
-main()
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(
+    `🚂 Express is running on port http://localhost:${port}/graphql!`
+  );
+});
